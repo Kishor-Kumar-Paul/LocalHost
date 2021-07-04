4b353e40a8ef48c4eb1496df6a587e5c09e96b9b
Kishor-Kumar-Paul <81525911+Kishor-Kumar-Paul@users.noreply.github.com>
2021-07-04 21:05:41 +0600

    files added

---
+++ project.js
@@ -0,0 +1,10 @@
+< onclick= a();>
+function a(){
+    var r = confirm("Please Enter the allow buttom");
+    if (r == true){
+        alert("You are on!");} else{
+        alert("You are off!");
+    }
+   
+
+}
